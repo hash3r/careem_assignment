4e5ebd643b7ba45e9737ee4d1abe8c3530c0b227
Vlad Gnatiuk <gnatiuk.vladimir@gmail.com>
2018-07-17 13:22:25 +0200

    Add REST manager for processing requests

---
+++ careem_test/Network/Rest.swift
@@ -0,0 +1,71 @@
+//
+//  Rest.swift
+//  careem_test
+//
+//  Created by Hnatiuk, Volodymyr on 16.07.18.
+//  Copyright © 2018 careem. All rights reserved.
+//
+
+import Alamofire
+import AlamofireObjectMapper
+import ObjectMapper
+import SwiftyJSON
+import HTTPStatusCodes
+import PromiseKit
+
+
+/// REST singleton class for communicating with the services
+open class Rest {
+    
+    /// A shared instance of `RestAPI`, used by top-level request methods
+    static let shared = Rest()
+    
+    /// Alamofire manager  that responsible for creating and managing `Request` objects
+    fileprivate let manager = SessionManager()
+    
+    func mappableRequest<T: BaseMappable>(_ target: TargetType) -> Promise<T> {
+        
+        return Promise<T> { seal in
+            let router = Router(target)
+            let request = manager.request(router)
+//            debugPrint(request)
+            request.responseObject { (response: DataResponse<T>) -> Void in
+//                debugPrint(response.fullDescription)
+                if let value = response.result.value, response.isSuccess() {
+                    seal.fulfill(value)
+                } else {
+                    if let error = response.result.error {
+                        seal.reject(error)
+                    } else {
+                        seal.reject(self.defaultRestErrorWith(response.response?.statusCode))
+                    }
+                }
+            }
+        }
+    }
+    
+    func arrayMappableRequest<T: BaseMappable>(_ target: TargetType) -> Promise<[T]> {
+        
+        return Promise<[T]> { seal in
+            let router = Router(target)
+            let request = manager.request(router)
+//            debugPrint(request)
+            request.responseArray { (response: DataResponse<[T]>) -> Void in
+//                debugPrint(response.fullDescription)
+                if let value = response.result.value, response.isSuccess() {
+                    seal.fulfill(value)
+                } else {
+                    if let error = response.result.error {
+                        seal.reject(error)
+                    } else {
+                        seal.reject(self.defaultRestErrorWith(response.response?.statusCode))
+                    }
+                }
+            }
+        }
+    }
+    
+    fileprivate func defaultRestErrorWith(_ code: Int?) -> NSError {
+        return NSError(domain: Bundle.main.bundleIdentifier!, code: code ?? 0, userInfo: [NSLocalizedDescriptionKey: "See response above"])
+    }
+}
